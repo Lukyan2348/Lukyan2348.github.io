03991beedabc0bf77c1548b8b9f8dcf82214a9cd
Lukyan2348 <58484076+Lukyan2348@users.noreply.github.com>
2019-12-10 20:00:13 +0300

    change home link

---
+++ kontakti.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/stily.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Контактная форма</title>
+</head>
+<body>
+  <div class="container mt-5">
+  <h3>Контактная форма</h3>
+  <form action="check.php" method="post">
+    <input type="email" name="email" placeholder="Введите Email" class="form-control"><br>
+    <textarea name="message" class="form-control" placeholder="Введите ваше сообщение"></textarea><br>
+    <button type="sumbit" name="send" class="btn btn-success">Отправить</button>
+  </form>
+  </div>
+</body>
+</html>
